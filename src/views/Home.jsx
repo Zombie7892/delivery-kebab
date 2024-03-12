a29a6beed94f5bac2bb5405e68345e15c3915e0e
VictoriyaD1 <vikaptitsa@gmail.com>
2024-03-12 12:58:08 +0300

    Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,38 @@
-const React = require('react');
-const Layout = require('./Layout');
+const React = require("react");
+const Layout = require("./Layout");
 
-module.exports = function Home({ login }) {
+module.exports = function Home({ login, products }) {
   return (
     <Layout login={login}>
-      <div className="container homePage">
-        <h1>Добро пожаловать в delivery cebab!</h1>
+      <div className="catalogCards" style={{ marginTop: "1%" }}>
+        {/* {products.reverse().map((product) => ( */}
+        <div className="card" style={{ width: "18rem" }}>
+          <img className="card-img-top" src="#" alt="Фото товара" />
+
+          <div className="card-body">
+            <h5 className="card-title" style={{ color: "#DC143C" }}>
+              (Новая цена) ₽
+            </h5>
+            <h5
+              className="card-title"
+              style={{ textDecoration: "line-through" }}
+            >
+              (Исходная цена) ₽
+            </h5>
+            {/* { роль-клиент ? ( 
+              
+             ) : ( 
+             <>  */}
+
+            {"  "}
+            <a href="#" type="button" className="btn btn-outline-info">
+              Выкупить
+            </a>
+            {/* </>
+                )} */}
+          </div>
+        </div>
+        {/* ))} */}
       </div>
     </Layout>
   );
