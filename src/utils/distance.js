909073ee65dcb09552f365bedee260433cfa8659
Dmitrii <skeleton007@yandex.ru>
2024-03-12 20:36:38 +0300

    time formula and new seed

--- src/utils/distance.js
+++ src/utils/distance.js
@@ -1,4 +1,6 @@
-module.exports = function calcCrow(lat1, lon1, lat2, lon2) {
+const timeFromDistance = require('./timeFromDistance');
+
+function calcCrow(lat1, lon1, lat2, lon2, speed = 20) {
   function toRad(Value) {
     return Value * Math.PI / 180;
   }
@@ -12,10 +14,12 @@ module.exports = function calcCrow(lat1, lon1, lat2, lon2) {
     + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const d = R * c;
-  return d;
-}
+  return {
+    distance: d.toFixed(1),
+    time: timeFromDistance(d, speed).toFixed(0),
+  };
+};
 
+module.exports = calcCrow;
 
-
-/* console.log(calcCrow(55.717359, 37.895778, 33.520070, -101.869721));
- */
+// console.log(calcCrow(55.717359, 37.895778, 55.72397, 37.575452, 30));
