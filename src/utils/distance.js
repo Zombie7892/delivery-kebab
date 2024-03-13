e249865dc31979510c64e334bfbdd9963d82f2f5
Dmitrii <skeleton007@yandex.ru>
2024-03-13 14:11:47 +0300

    render map

--- src/utils/distance.js
+++ src/utils/distance.js
@@ -14,12 +14,50 @@ function calcCrow(lat1, lon1, lat2, lon2, speed = 20) {
     + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const d = R * c;
+
+  let distanceTimeFactor = 1.5;
+  if (d >= 0 && d <= 5) {
+    distanceTimeFactor = 1.5;
+  }
+  if (d > 5 && d <= 7) {
+    distanceTimeFactor = 1.6;
+  }
+  if (d > 7 && d <= 10) {
+    distanceTimeFactor = 1.7;
+  }
+  if (d > 10 && d <= 15) {
+    distanceTimeFactor = 1.8;
+  }
+  if (d > 15 && d <= 20) {
+    distanceTimeFactor = 1.9;
+  }
+  if (d > 20 && d <= 30) {
+    distanceTimeFactor = 2;
+  }
+  if (d > 30) {
+    distanceTimeFactor = 2.1;
+  }
+
+  let dayPeriodFactor = 1;
+  if (new Date().getHours() >= 6 && new Date().getHours() < 12) {
+    dayPeriodFactor = 1.5;
+  }
+  if (new Date().getHours() >= 12 && new Date().getHours() < 18) {
+    dayPeriodFactor = 1.5;
+  }
+
+  const time = timeFromDistance(d, speed, distanceTimeFactor, dayPeriodFactor).toFixed();
+
+  const timeHours = Math.floor(time / 60);
+  const timeMinutes = time % 60;
+
   return {
     distance: d.toFixed(1),
-    time: timeFromDistance(d, speed).toFixed(0),
+    timeHours,
+    timeMinutes,
   };
-};
+}
 
 module.exports = calcCrow;
 
-// console.log(calcCrow(55.717359, 37.895778, 55.72397, 37.575452, 30));
+// console.log(calcCrow(55.717312, 37.895771, 55.727343, 37.649469, 30));
