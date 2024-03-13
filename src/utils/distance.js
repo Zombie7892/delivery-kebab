b32b2346113c9a5e77d62dbf1b03069b5c4f3660
Dmitrii <skeleton007@yandex.ru>
2024-03-13 15:37:32 +0300

    distance up

--- src/utils/distance.js
+++ src/utils/distance.js
@@ -15,35 +15,35 @@ function calcCrow(lat1, lon1, lat2, lon2, speed = 20) {
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const d = R * c;
 
-  let distanceTimeFactor = 1.5;
+  let distanceTimeFactor = 1.3;
   if (d >= 0 && d <= 5) {
-    distanceTimeFactor = 1.5;
-  }
-  if (d > 5 && d <= 7) {
     distanceTimeFactor = 1.6;
   }
+  if (d > 5 && d <= 7) {
+    distanceTimeFactor = 1.5;
+  }
   if (d > 7 && d <= 10) {
-    distanceTimeFactor = 1.7;
+    distanceTimeFactor = 1.5;
   }
   if (d > 10 && d <= 15) {
-    distanceTimeFactor = 1.8;
+    distanceTimeFactor = 1.4;
   }
   if (d > 15 && d <= 20) {
-    distanceTimeFactor = 1.9;
+    distanceTimeFactor = 1.4;
   }
   if (d > 20 && d <= 30) {
-    distanceTimeFactor = 2;
+    distanceTimeFactor = 1.2;
   }
   if (d > 30) {
-    distanceTimeFactor = 2.1;
+    distanceTimeFactor = 1.2;
   }
 
   let dayPeriodFactor = 1;
   if (new Date().getHours() >= 6 && new Date().getHours() < 12) {
-    dayPeriodFactor = 1.5;
+    dayPeriodFactor = 1.2;
   }
-  if (new Date().getHours() >= 12 && new Date().getHours() < 18) {
-    dayPeriodFactor = 1.5;
+  if (new Date().getHours() >= 17 && new Date().getHours() < 20) {
+    dayPeriodFactor = 1.2;
   }
 
   const time = timeFromDistance(d, speed, distanceTimeFactor, dayPeriodFactor).toFixed();
